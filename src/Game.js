b6c8f1b4a71b6fdd31d920edc3663c8caf13098e
David Evans <david0evans@gmail.com>
2013-06-07 19:13:22 -0700

    Spawn humans on edges

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,13 @@ Game.prototype.update = function(dt)
     {
       var obj = JSEngine.factory.createObject();
       obj.components.human = new Human(obj);
+      obj.components.cube = new Cube(obj, { color:0x00FF00 });
+      obj.position.y = 1;
+      if (Math.random() < 0.5)
+        obj.position.x = -20;
+      else
+        obj.position.x = 20;
+      obj.position.z = Math.round(-5 + Math.random() * 10);
     }
   }
 }
