012868f6b32f18ed566a1f1406c75b60da8c8ead
Killian Koenig <killiankoenig@gmail.com>
2013-06-08 13:46:10 -0700

    Added perf monitor

--- src/Engine.js
+++ src/Engine.js
@@ -9,6 +9,15 @@ function Engine()
   this.graphics = new Graphics();
 
   this.lastTime = new Date();
+
+  this.stats = new Stats();
+  this.stats.domElement.style.position = 'absolute';
+  this.stats.domElement.style.top = '0px';
+  this.stats.domElement.style.zIndex = 100;
+
+  var container = document.createElement('div');
+  container.appendChild(this.stats.domElement);  
+  document.body.appendChild(container);
 }
 
 Engine.prototype.start = function()
@@ -31,4 +40,6 @@ function engineUpdate()
   JSEngine.factory.update(dt);
   JSEngine.game.update(dt);
   JSEngine.graphics.update(dt);
+
+  JSEngine.stats.update();
 }
