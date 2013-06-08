64391684e792ed778c158c7706fef5d777db48aa
David Evans <david0evans@gmail.com>
2013-06-07 19:27:38 -0700

    Modify human spawn

--- src/Game.js
+++ src/Game.js
@@ -16,9 +16,9 @@ Game.prototype.update = function(dt)
       obj.components.cube = new Cube(obj, { color:0x00FF00 });
       obj.position.y = 1;
       if (Math.random() < 0.5)
-        obj.position.x = -30;
+        obj.position.x = -30 + Math.random() * 5;
       else
-        obj.position.x = 30;
+        obj.position.x = 30 - Math.random() * 5;
       obj.position.z = Math.round(-10 + Math.random() * 20);
     }
   }
