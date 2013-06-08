573a75f06686f1a2b11b0b048087d2876431b89e
David Evans <david0evans@gmail.com>
2013-06-08 12:23:00 -0700

    Turning off 2 player for now

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,7 @@ function main()
   var e = new Engine();
 
   var player1 = createPlayer(0, 0xFF0000, 0.0);
-  var player2 = createPlayer(1, 0xFF0000, 0.5);
+  // var player2 = createPlayer(1, 0xFF0000, 0.5);
 
   e.start();
 }
