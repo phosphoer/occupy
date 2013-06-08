40d0e283153d1fd181c9fddbb1e1b40993702e31
Killian Koenig <killiankoenig@gmail.com>
2013-06-07 17:39:01 -0700

    Working on ground.

--- src/Graphics.js
+++ src/Graphics.js
@@ -2,10 +2,30 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
 
+
+var levelCubes;
+
+function createLevelChunk(xWidth, zWidth, height)
+{
+    var level = [];
+    for (var i = 0; i < xWidth; ++i)
+    {
+        level.push([]);
+
+        for(var j = 0; j < zWidth; ++j)
+        {
+            level[i][j] = 1;
+        }
+    }
+
+    return level;
+}
+
 function init() {
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 10;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
 
     scene = new THREE.Scene();
 
@@ -15,7 +35,7 @@ function init() {
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    var light = new THREE.AmbientLight(0x101010);
+    var light = new THREE.AmbientLight(0x404040);
     //scene.add(light);
 
     light = new THREE.PointLight( 0xFFFFFF, 1, 1000 );
@@ -29,6 +49,27 @@ function init() {
 
     document.body.appendChild( renderer.domElement );
 
+    var levelWidth = 40;
+    var levelDepth = 10;
+    var levelChunk = createLevelChunk(levelWidth, levelDepth, 1);
+    for(var i = 0; i < levelWidth; ++i)
+    {
+        for(var j = 0; j < levelDepth; ++j)
+        {
+            if(levelChunk[i][j])
+            {
+                var cube = new THREE.Mesh( geometry, material );
+                cube.position = new THREE.Vector3(i, 0, j);
+                mesh.add(cube);
+            }
+        }
+    }
+
+    camera.position.z = 40;
+    camera.position.y = 10;
+    camera.position.x = levelWidth / 2;
+
+
     animate();
 
 }
@@ -38,8 +79,8 @@ function animate() {
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    //mesh.rotation.x += 0.01;
+    //mesh.rotation.y += 0.02;
 
     renderer.render( scene, camera );
 
@@ -52,4 +93,4 @@ function onWindowResize(){
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-}
+}   
