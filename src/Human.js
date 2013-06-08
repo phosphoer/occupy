af433a3b41794f7b7d13c54bd7d426d35fd64ec9
David Evans <david0evans@gmail.com>
2013-06-07 19:23:36 -0700

    Humans go to center

--- src/Human.js
+++ src/Human.js
@@ -8,4 +8,7 @@ function Human(parent)
 
 Human.prototype.update = function(dt)
 {
+  var angleToCenter = Math.atan2(-this.parent.position.z, -this.parent.position.x);
+  this.parent.position.x += Math.cos(angleToCenter) * this.movementSpeed * dt;
+  this.parent.position.z += Math.sin(angleToCenter) * this.movementSpeed * dt;
 }
