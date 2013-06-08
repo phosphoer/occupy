72b27f7e8719aba5fc93b8e38b8ae10552946521
Killian Koenig <killiankoenig@gmail.com>
2013-06-08 12:58:59 -0700

    Fixed tower.

--- src/Main.js
+++ src/Main.js
@@ -5,5 +5,12 @@ function main()
   var player1 = createPlayer(0, 0x440000, 0.0);
   // var player2 = createPlayer(1, 0xFF0000, 0.5);
 
+  var tower = e.factory.createObject();
+  tower.components.tower = new Tower(tower);
+
+  // center tower on ground
+  tower.position.set(-tower.components.tower.sizeX / 2, 1,
+                     -tower.components.tower.sizeZ / 2);
+
   e.start();
 }
--- src/Tower.js
+++ src/Tower.js
@@ -1,10 +1,44 @@
+
+
 function Tower(parent)
 {
   this.parent = parent;
   this.health = 1;
-  this.height = 1;
+
+  this.base = new THREE.Object3D();
+  this.buildFromAsset(ZeBestTower);
+  JSEngine.graphics.scene.add(this.base);
+  this.base.position = this.parent.position;
 }
 
 Tower.prototype.update = function(dt)
 {
+}
+
+Tower.prototype.buildFromAsset = function(asset)
+{
+    var geometry = new THREE.CubeGeometry(1, 1, 1);
+
+    this.sizeX = asset.x;
+    this.sizeY = asset.y;
+    this.sizeZ = asset.z;
+    
+    // load mesh from file
+    for(var j = 0; j < asset.y; ++j)
+    {
+        for(var k = 0; k < asset.z; ++k)
+        {
+            for(var i = 0; i < asset.x; ++i)
+            {
+                var cubeColor = asset.data[j * asset.x * asset.z + k * asset.x + i];
+                if(cubeColor != 0)
+                {      
+                    var material = new THREE.MeshLambertMaterial( { color: cubeColor} );
+                    var mesh = new THREE.Mesh(geometry, material);
+                    mesh.position = new THREE.Vector3(i, asset.y - j - 1, k);
+                    this.base.add(mesh);
+                }   
+            }
+        }
+    }
 }
