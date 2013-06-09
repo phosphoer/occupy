68f7e813220c1b49dd02f18b1449076569bf150b
David Evans <david0evans@gmail.com>
2013-06-09 04:46:46 -0700

    game pauses during main menu

--- src/Game.js
+++ src/Game.js
@@ -38,14 +38,16 @@ function Game()
 
 
   this.mainMenuContainer = $("<div class='MainMenuContainer'></div>").appendTo($("body"));
-  
+
   $("<div class='MainMenuTitle'>#Occupy Vampire Wallstreet</div>").appendTo(this.mainMenuContainer);
   var mainMenuBody = $("<div class='MainMenuBody'></div>").appendTo(this.mainMenuContainer);
   $("<div class='Agreement'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</div>").appendTo(mainMenuBody);
   var startGame = $("<div class='Button'>I Accept</div>").appendTo(mainMenuBody);
   var that = this;
-  startGame.bind('click', function () 
+  JSEngine.stop();
+  startGame.bind('click', function ()
   {
+    JSEngine.start();
     that.mainMenuContainer.remove();
   });
 
